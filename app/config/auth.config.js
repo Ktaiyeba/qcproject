0bbe17bde83bb37d9b49a6b6100a1c5a409dfe92
Ktaiyeba <78835324+Ktaiyeba@users.noreply.github.com>
2021-05-23 16:02:49 -0400

    Delete app directory

--- app/config/auth.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    secret : "Khadija-secret-key"
-}
--- app/controllers/index.js
+++
@@ -1,6 +0,0 @@
-const userCont = require('./user.controller');
-const courseCont = require('./course.controller');
-module.exports = {
-    userCont,
-    courseCont
-};
